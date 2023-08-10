21f699b6df04df4ce499d0482a34cbbd32266cc4
mahdizynali <zeinali.mahdy@gmail.com>
2023-08-11 01:19:03 +0330

    setup configuration

--- trainer.py
+++ trainer.py
@@ -7,9 +7,9 @@ from utils.coEFFMatrix import machinLearningMatrix as ml
 from config import HyperParameters, IMG_SIZE, SAVE_LOG_PATH, SAVE_MODEL_PATH
 hyper = HyperParameters()
 
-training_generator = DataGenerator(train_ids, batch_size=hyper.batchSize, n_channels=2)
-valid_generator = DataGenerator(val_ids)
-test_generator = DataGenerator(test_ids)
+training_generator = DataGenerator(train_ids, batch_size=hyper.batchSize, n_channels=2, shuffle=True)
+valid_generator = DataGenerator(val_ids, batch_size=hyper.batchSize, n_channels=2, shuffle=True)
+test_generator = DataGenerator(test_ids, batch_size=hyper.batchSize, n_channels=2, shuffle=True)
 
 model = attUnet((IMG_SIZE, IMG_SIZE, 2), hyper.modelKernel, hyper.modelDropout).generateModel()
 model.compile(
