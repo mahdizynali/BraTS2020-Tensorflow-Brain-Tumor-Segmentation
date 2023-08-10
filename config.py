081d7fafbab3924d58cf336e84e84132b204a5b1
mahdizynali <zeinali.mahdy@gmail.com>
2023-08-11 01:23:38 +0330

    setup configuration

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ NUM_CLASSES=4
 IMG_SIZE=128
 TRAIN_DATASET_PATH = '/media/mahdi/individual/dataset/MICCAI_BraTS2020_TrainingData/'
 VALIDATION_DATASET_PATH = '/media/mahdi/individual/dataset/MICCAI_BraTS2020_ValidationData/'
-PRE_TRAINED_MODEL_PATH = '/home/mahdi/Desktop/tf2/first_train/model-agust.h5'
-PRE_TRAINED_LOG_PATH = '/home/maximum/Desktop/tf2/first_train/training.log'
 SAVE_MODEL_PATH = 'result-model.h5'
 SAVE_LOG_PATH = 'training.log'
+
+# if they are existed before:
+PRE_TRAINED_MODEL_PATH = '/home/mahdi/Desktop/tf2/first_train/model-agust.h5'
+PRE_TRAINED_LOG_PATH = '/home/maximum/Desktop/tf2/first_train/training.log'
 
 
 class HyperParameters:
