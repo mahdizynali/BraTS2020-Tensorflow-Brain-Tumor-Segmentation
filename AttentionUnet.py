69e7c7b6c2150213fb834e07d56ebe5441e35692
mahdizynali <zeinali.mahdy@gmail.com>
2023-08-14 13:59:55 +0330

    testing more models

--- AttentionUnet.py
+++ AttentionUnet.py
@@ -67,6 +67,87 @@ class attUnet:
         conv10 = Conv2D(4, (1, 1), activation='softmax')(conv)
         
         return Model(inputs = self.input_layers, outputs = conv10)
+
+
+
+
+class attUnet2():
+    '''simple unet model'''
+
+    def __init__(self, inp, kernel, dropout):
+        self.kernel = kernel
+        self.dropout = dropout
+        self.input_layer = Input(shape=inp)
+        self.generateModel()
+
+    def generateModel(self) -> Model :
+        conv1 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(self.input_layer)
+        conv1 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv1)
+        conv1 = BatchNormalization()(conv1)
+        
+        mxpool = MaxPooling2D(pool_size=(2, 2))(conv1)
+        conv = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(mxpool)
+        conv = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv)
+        conv = BatchNormalization()(conv)
+        
+        mxpool1 = MaxPooling2D(pool_size=(2, 2))(conv)
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(mxpool1)
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv2)
+        conv2 = BatchNormalization()(conv2)
+        
+        mxpool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(mxpool2)
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv3)
+        conv3 = BatchNormalization()(conv3)
+        
+        mxpool4 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        conv5 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(mxpool4)
+        conv5 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv5)
+        conv5 = BatchNormalization()(conv5)
+        drop5 = Dropout(self.dropout)(conv5)
+
+        up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer=self.kernel)(UpSampling2D(size=(2, 2))(drop5))
+        merge7 = concatenate([conv3, up7], axis=3)
+        att7 = self.attention_block(merge7, conv3)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(att7)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv7)
+
+        up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer=self.kernel)(UpSampling2D(size=(2, 2))(conv7))
+        merge8 = concatenate([conv2, up8], axis=3)
+        att8 = self.attention_block(merge8, conv2)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(att8)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv8)
+
+        up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer=self.kernel)(UpSampling2D(size=(2, 2))(conv8))
+        merge9 = concatenate([conv, up9], axis=3)
+        att9 = self.attention_block(merge9, conv)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(att9)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv9)
+        
+        up = Conv2D(32, 2, activation='relu', padding='same', kernel_initializer=self.kernel)(UpSampling2D(size=(2, 2))(conv9))
+        merge = concatenate([conv1, up], axis=3)
+        conv = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(merge)
+        conv = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer=self.kernel)(conv)
+        
+        conv10 = Conv2D(4, (1, 1), activation='softmax')(conv)
+        
+        return Model(inputs=self.input_layer, outputs=conv10)
+
+    def attention_block(self, x, shortcut):
+        g1 = Conv2D(x.shape[-1] // 8, kernel_size=(1, 1), strides=(1, 1), padding='same', kernel_initializer=self.kernel)(shortcut)
+        g1 = BatchNormalization()(g1)
+        x1 = Conv2D(x.shape[-1] // 8, kernel_size=(1, 1), strides=(1, 1), padding='same', kernel_initializer=self.kernel)(x)
+        x1 = BatchNormalization()(x1)
+        theta_x = Conv2D(x.shape[-1] // 8, kernel_size=(1, 1), strides=(1, 1), padding='same', kernel_initializer=self.kernel)(x)
+        theta_x = BatchNormalization()(theta_x)
+        phi_g = Conv2D(x.shape[-1] // 8, kernel_size=(1, 1), strides=(1, 1), padding='same', kernel_initializer=self.kernel)(g1)
+        phi_g = BatchNormalization()(phi_g)
+        f = Add()([theta_x, phi_g])
+        f = Conv2D(1, kernel_size=(1, 1), strides=(1, 1), padding='same', kernel_initializer=self.kernel)(f)
+        f = BatchNormalization()(f)
+        y = Multiply()([f, g1])
+        y = Add()([y, shortcut])
+        return y
     
 #========================================================== 
 class simpleUnet():
